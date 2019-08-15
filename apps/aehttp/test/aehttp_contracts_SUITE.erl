6e02c9144cd50d72c38c92ef5ce9db653bf271a6
Grzegorz Uriasz <gorbak25@gmail.com>
2019-08-15 19:20:38 +0200

    Memoize expensive contract operations

--- apps/aehttp/test/aehttp_contracts_SUITE.erl
+++ apps/aehttp/test/aehttp_contracts_SUITE.erl
@@ -53,7 +53,7 @@
 -define(DEFAULT_TESTS_COUNT, 5).
 -define(DEFAULT_GAS_PRICE, aec_test_utils:min_gas_price()).
 
--define(MAX_MINED_BLOCKS, 20).
+-define(MAX_MINED_BLOCKS, 100).
 
 all() ->
     [ {group, aevm}
@@ -104,7 +104,7 @@ init_per_group(VM, Config) when VM == aevm; VM == fate ->
 init_per_vm(Config) ->
     NodeName = aecore_suite_utils:node_name(?NODE),
     aecore_suite_utils:start_node(?NODE, Config),
-    aecore_suite_utils:connect(NodeName),
+    aecore_suite_utils:connect(NodeName, [block_pow]),
 
     ToMine = max(0, aecore_suite_utils:latest_fork_height()),
     ct:pal("ToMine ~p\n", [ToMine]),
